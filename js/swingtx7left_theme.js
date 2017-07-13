e81b2288b30590559e9f10f14e59e7e0ce8a2412
James Sinton <jksinton@gmail.com>
2017-07-13 00:53:30 -0500

    Frontpage and menu updates

--- js/swingtx7left_theme.js
+++ js/swingtx7left_theme.js
@@ -1,56 +1,60 @@
 /**
  * @file
- * Contains JavaScript used in Swing TX-07 Left theme.
+ * Contains JavaScript used in Swing TX-07 Left's theme.
  */
 
 (function ($) {
 
-  'use strict';
+	'use strict';
 
-  // Responsive main menu.
-  $('#main-menu').smartmenus();
+	$(document).ready(function() {
+		$("#main-menu").mmenu({
+			"extensions": ["listview-justified"]	
+		});
+	});
 
-  // Main menu toggle.
-  $('.navbar-toggle').click(function () {
-    $('.region-primary-menu').slideToggle();
-  });
+	var $menu = $("#main-menu").mmenu({
+	   //   options
+	   "extensions": ["listview-justified"]	
+	});
+	var $icon = $("#my-icon");
+	var API = $menu.data( "mmenu" );
 
-  // Hide dropdown menu.
-  if ($(window).width() < 768) {
-    $('.region-primary-menu li a:not(.has-submenu)').click(function () {
-      $('.region-primary-menu').hide();
-    });
-  }
+	$icon.on( "click", function() {
+	   API.open();
+	});
 
-  // Sliding header.
-  $('.toggle-switch').click(function () {
-    $('#sliding-header-wrap').slideToggle();
-  });
+	API.bind( "open:finish", function() {
+	   setTimeout(function() {
+		  $icon.addClass( "is-active" );
+	   }, 100);
+	});
+	API.bind( "close:finish", function() {
+	   setTimeout(function() {
+		  $icon.removeClass( "is-active" );
+	   }, 100);
+	});
 
-  // Bootstrap tooltip.
-  $('[data-toggle=tooltip]').tooltip();
+	// Bootstrap tooltip.
+	$('[data-toggle=tooltip]').tooltip();
 
-  // Smooth scroll to top.
-  $('#toTop a').click(function () {
-    $('body,html').animate({scrollTop: 0}, 1000);
-  });
+	// Smooth scroll to top.
+	$('#toTop a').click(function () {
+		$('body,html').animate({scrollTop: 0}, 1000);
+	});
 
-/*
-  $(document).scroll(function(e){
-	var target = $('.caption div.border');
-	var scrollTop = $(this).scrollTop();
-	var height = target.outerHeight();
-	var calc = 1 - (scrollTop - height) / height;
+	$(document).scroll(function(e){
+		var target = $('.caption');
+		var scrollTop = $(this).scrollTop();
+		var height = target.outerHeight();
+		var calc = 1 - (scrollTop - height) / height;
 
-	target.css({ 'opacity': calc });
+		target.css({ 'opacity': calc });
 
-	if ( calc > '1' ) {
-		target.css({ 'opacity': 1 });
-	} else if ( calc < '0' ) {
-		target.css({ 'opacity': 0 });
-	}
-  });
-  */
+		if ( calc > '1' ) {
+			target.css({ 'opacity': 1 });
+		} else if ( calc < '0' ) {
+			target.css({ 'opacity': 0 });
+		}
+	});
 })(jQuery);
-
-
